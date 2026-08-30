5794c13fd6f4d746524df5bf8826aaa9b038d077
yoeelfakhry <yoeelfakhry88@gmail.com>
2026-08-30 19:27:40 +0200

    try rouge manual after train

--- train.py
+++ train.py
@@ -2,8 +2,8 @@ from datasets import load_dataset
 from transformers import Seq2SeqTrainer , Seq2SeqTrainingArguments
 from src.utils.config_loader import load_config
 from src.utils.data_preprocessing import get_tokenizer , preprocess_data , get_data_collator
-from src.models.bart_model import load_bart_model
-from src.eval.rouge_eval import compute_metrics_fn
+from src.models.bart_model import load_bart_model   
+# from src.eval.rouge_eval import compute_metrics_fn
 import torch
 from kaggle_secrets import UserSecretsClient
 from huggingface_hub import login
@@ -73,7 +73,7 @@ training_args = Seq2SeqTrainingArguments(
     push_to_hub=True,
     eval_accumulation_steps=config["eval_accumulation_steps"],
 
-)   
+)
 
 print("=== DEBUG ===")
 print("predict_with_generate:", training_args.predict_with_generate, type(training_args.predict_with_generate))
@@ -104,4 +104,44 @@ print(f"Training complete. Final model saved to: {final_model_dir}")
 print(train_result)
 
 
-   
+import evaluate
+import numpy as np
+
+
+def compute_metrics_fn(tokenizer):
+    """
+    Create a function to compute the ROUGE score for the predicted summaries against the reference summaries.
+    Args:
+        tokenizer: The tokenizer object for the specified model 
+    Returns:
+        function: A function that takes in a tuple of predicted summaries and reference summaries and returns a dictionary containing the ROUGE scores for the predicted summaries against the reference summaries
+    """
+
+    rouge_score  = evaluate.load("rouge")
+
+    def compute_metrics(eval_pred):
+        """
+        Compute the ROUGE score for the predicted summaries against the reference summaries.
+        
+        Args:
+        eval_pred (tuple): A tuple containing the predicted summaries and the reference summaries
+        Returns:
+        dict: A dictionary containing the ROUGE scores for the predicted summaries against the reference summaries 
+        """
+        print(">>> compute_metrics WAS CALLED <<<")
+        predictions,labels = eval_pred 
+
+        print("DEBUG predictions type:", type(predictions))
+        if isinstance(predictions, tuple):
+            print("DEBUG predictions is tuple, length:", len(predictions))
+            predictions = predictions[0]
+
+        predictions = np.where(predictions != -100 , predictions , tokenizer.pad_token_id)
+        labels = np.where(labels != -100 , labels , tokenizer.pad_token_id)
+
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        result = rouge_score.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+
+        return { k: v*100 for k , v in result.items()}  
